42870d0aa5db74fdb0b4c01a673db4196c743201
OleStan <59766354+OleStan@users.noreply.github.com>
2020-11-08 00:12:10 +0200

    scetch

--- HW2/Tamagochi.rb
+++ HW2/Tamagochi.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class Pet
-  def initialize (animal, name, stomach, hp, interest, sleepiness, intelect, purity)
+  attr_accessor :animal, :name, :stomach, :hp, :interest, :sleepiness, :intelect, :purity
+
+  def initialize(animal, name, stomach, hp, interest, sleepiness, intelect, purity)
     @animal = animal
     @name = name
     @hp = hp
@@ -10,58 +14,6 @@ class Pet
     @purity = purity
   end
 
-  def voice
-    case @animal
-    when "cat"
-      'meow'
-    when "dog"
-      'bark'
-    when "hamster"
-      'pee-pee'
-    end
-  end
-
-  # action------------------------------------------
-  def feed()
-
-  end
-  def play
-
-  end
-  def wash
-
-  end
-  def look_at
-
-  end
-  def walk
-
-  end
-  def put_to_sleep
-
-  end
-  def train
-
-  end
-
-
-
-
-
-
-
-end
-
-
-class Timer
-
-end
-
-
-class Dog < Pet
-  def voice
-    "Bark"
-  end
   def render
     "         ,
             |`-.__
@@ -73,134 +25,112 @@ class Dog < Pet
      `l     /_ll
        `~~~~~``~`"
   end
-end
 
-class Cat < Pet
   def voice
-    "Meiw"
+    case @animal
+    when 'cat'
+      'meow'
+    when 'dog'
+      'bark'
+    when 'hamster'
+      'pee-pee'
+    else 'hello'
+    end
   end
-  def render
-    "       ,
-       l`-._           __
-        \\  `-..____,.'  `.
-         :`.         /    l`.
-         :  )       :      : l
-          ;'        '   ;  |  :
-          )..      .. .:.`.;  :
-         /::...  .:::...   ` ;
-         ; _ '    __        /:l
-         `:o>   /lo_>      ;:. `.
-        `-`.__ ;   __..--- /:.   l
-        === l_/   ;=====_.':.     ;
-         ,/'`--'...`--....        ;
-              ;                    ;
-            .'                      ;
-          .'                        ;
-        .'     ..     ,      .       ;
-       :       ::..  /      ;::.     |
-      /      `.;::.  |       ;:..    ;
-     :         |:.   :       ;:.    ;
-     :         ::     ;:..   |.    ;
-      :       :;      :::....|     |
-      /l     ,/ l      ;:::::;     ;
-    .:. l:..|    :     ; '.--|     ;
-   ::.  :''  `-.,,;     ;'   ;     ;
-.-'. _.'l      / `;      l,__:      l
-`---'    `----'   ;      /    l,.,,,/
-                   `----`"
+
+  # action------------------------------------------
+  def feed
   end
-end
 
-class Humster < Pet
-  def voice
-    "peek-peek"
+  def play; end
+
+  def wash; end
+
+  def look_at; end
+
+  def walk; end
+
+  def put_to_sleep; end
+
+  def train; end
+
+  class Timer
+    def initialize
+      @day_time = day_time
+    end
+    def day_period(day_time)
+      case day_time
+      when 1
+        'Morning'
+      when 2
+        'Noonday'
+      else
+        'Evening'
+
+      end
+    end
   end
-  def render
-    "             .     .
-            (>l---/<)
-            ,'     `.
-           /  q   p  l
-          (  >(_Y_)<  )
-           >-' `-' `-<-.
-          /  _.== ,=.,- l
-         /,    )`  '(    )
-        ; `._.'      `--<
-       :     l        |  )
-       l      )       ;_/
-        `._ _/_  ___.'-l
-           `--l"
-  end
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# action------------------------------------------
-def feed
-
-end
-def play
-
-end
-def wash
-
-end
-def look_at
-
-end
-def walk
-
-end
-def put_to_sleep
-
-end
-def train
-
 end
 
 
 
 # start game--------------------------------------
+
+def dayli(pet)
+  puts "---------------------------
+ Chose what you wanna do
+1.feed             5.walk
+2.play             6.put_to_sleep
+3.wash             7.train
+4.look at(nothing)
+Comand: "
+  input_action = gets.chomp.to_f
+  case input_action
+  when 1
+    pet.feed
+  when 2
+    pet.play
+  when 3
+    pet.wash
+  when 4
+    pet.look_at
+  when 5
+    pet.walk
+  when 6
+    pet.put_to_sleep
+  when 7
+    pet.train
+  else
+    puts "    !!!!!!!!!!!!!!!!!!!!!!!!
+    !! Type corect number !!
+    !!!!!!!!!!!!!!!!!!!!!!!!"
+    dayli(pet)
+  end
+end
+
 def start_game
-
+  print ' chose you animal name:  '
+  input_pet_name = gets.chomp
+  pet = Pet.new('dog', input_pet_name.capitalize, 5, 10, 4, 4, 2, 6)
+  timer = Timer.new('1')
+  puts " #{pet.render}"
+  puts "#{pet.name} say #{pet.voice}"
+  status(pet)
+  game(pet)
 end
 
-
-
-def game
-
+def status(pet, timer)
+  puts " #{pet.name}  stat
+Time = #{}
+---------------------------------------------
+|hp| = #{pet.hp}     |stomach| = #{pet.stomach}    |purity| = #{pet.purity}
+|interes| = #{pet.interest} |sleepiness| = #{pet.sleepiness} |intelect| = #{pet.intelect}
+--------------------------------------------- "
 end
 
-
-
-
-
-
-print " chose you animal"
-input_pet_choise = gets.chomp
-
-case input_pet_choise
-when 1
-  pet = Dog.new('dog', 'sdsd', )
-when 2
-  pet = Humster.new('cat', 'sdsd')
-when 3
-  pet = Humster.new('humster', 'sdsd')
+def game(pet)
+  dayli(pet)
 end
 
+start_game
 
-
-pet = Humster.new('cat', 'sdsd')
-puts " #{pet.voice}"
-puts " #{pet.render}"
