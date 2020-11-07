f3220aee42e6799bf716eeba69b3a51cc34281b0
OleStan <59766354+OleStan@users.noreply.github.com>
2020-11-07 16:59:26 +0200

    scetch

---
+++ HW2/Tamagochi.rb
@@ -0,0 +1,105 @@
+class Pet
+  def initialize
+    @hp = hp
+    @interest = interest
+    @stomach = stomach
+    @sleepiness = sleepiness
+    @intelect = intelect
+    @purity = purity
+  end
+end
+
+class Time
+
+end
+
+
+class Dog < Pet
+  def voice
+    "Bark"
+  end
+=begin
+  __      _
+o'')}____//
+ `_/      )
+ (_(_/-(_/
+=end
+end
+
+class Cat < Pet
+  def voice
+    "Meiw"
+  end
+=begin
+   |\---/|
+   | ,_, |
+    \_`_/-..----.
+ ___/ `   ' ,""+ \
+(__...'   __\    |`.___.';
+  (_,...'(_,.`__)/'.....+
+=end
+end
+
+class Humster < Pet
+  def voice
+    "peek-peek"
+  end
+=begin
+             .     .
+            (>\---/<)
+            ,'     `.
+           /  q   p  \
+          (  >(_Y_)<  )
+           >-' `-' `-<-.
+          /  _.== ,=.,- \
+         /,    )`  '(    )
+        ; `._.'      `--<
+       :     \        |  )
+       \      )       ;_/  hjw
+        `._ _/_  ___.'-\\\
+           `--\\\
+=end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# action------------------------------------------
+def feed
+
+end
+def play
+
+end
+def wash
+
+end
+def look_at
+
+end
+def
+
+
+
+
+# start game--------------------------------------
+def start_game
+
+end
+
+
+
+def game
+
+end
