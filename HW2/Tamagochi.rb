6d408eccb387fefc85bdb6c95fdcbae83646b407
OleStan <59766354+OleStan@users.noreply.github.com>
2020-11-09 19:07:59 +0200

    not prety but done

--- HW2/Tamagochi.rb
+++ HW2/Tamagochi.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
+class String
+  def black;          "\e[30m#{self}\e[0m" end
+  def red;            "\e[31m#{self}\e[0m" end
+  def green;          "\e[32m#{self}\e[0m" end
+  def brown;          "\e[33m#{self}\e[0m" end
+  def blue;           "\e[34m#{self}\e[0m" end
+  def magenta;        "\e[35m#{self}\e[0m" end
+  def cyan;           "\e[36m#{self}\e[0m" end
+  def gray;           "\e[37m#{self}\e[0m" end
 
+  def bg_black;       "\e[40m#{self}\e[0m" end
+  def bg_red;         "\e[41m#{self}\e[0m" end
+  def bg_green;       "\e[42m#{self}\e[0m" end
+  def bg_brown;       "\e[43m#{self}\e[0m" end
+  def bg_blue;        "\e[44m#{self}\e[0m" end
+  def bg_magenta;     "\e[45m#{self}\e[0m" end
+  def bg_cyan;        "\e[46m#{self}\e[0m" end
+  def bg_gray;        "\e[47m#{self}\e[0m" end
+
+  def bold;           "\e[1m#{self}\e[22m" end
+  def italic;         "\e[3m#{self}\e[23m" end
+  def underline;      "\e[4m#{self}\e[24m" end
+  def blink;          "\e[5m#{self}\e[25m" end
+  def reverse_color;  "\e[7m#{self}\e[27m" end
+end
 class Pet
   attr_accessor :animal, :name, :stomach, :hp, :interest, :sleepiness, :intelect, :purity
 
@@ -12,6 +36,7 @@ class Pet
     @sleepiness = sleepiness
     @intelect = intelect
     @purity = purity
+
   end
 
   def render
@@ -43,6 +68,7 @@ class Pet
     timer.day_time += 1
     puts "You feed #{pet.name} and now #{timer.day_period}"
     timer.status_counting(pet, timer, 'healing')
+    danger_notification(pet)
     status(pet, timer)
 
   end
@@ -51,6 +77,7 @@ class Pet
     timer.day_time += 1
     puts "You play with #{pet.name} and now #{timer.day_period}"
     timer.status_counting(pet, timer, 'playing')
+    danger_notification(pet)
     status(pet, timer)
   end
 
@@ -58,6 +85,7 @@ class Pet
     timer.day_time += 1
     puts "You wash #{pet.name} and now #{timer.day_period}"
     timer.status_counting(pet, timer, 'cleaning')
+    danger_notification(pet)
     status(pet, timer)
   end
 
@@ -65,6 +93,7 @@ class Pet
     timer.day_time += 1
     puts "You just look at #{pet.name} and now #{timer.day_period}"
     timer.status_counting(pet, timer, 'looking')
+    danger_notification(pet)
     status(pet, timer)
 
   end
@@ -73,13 +102,17 @@ class Pet
     timer.day_time += 1
     puts "You walk with #{pet.name} and now #{timer.day_period}"
     timer.status_counting(pet, timer, 'walking')
+    danger_notification(pet)
     status(pet, timer)
+    timer.endings(pet,timer, 'disappear_b') if interest? && rand(1..10) == 3
+    timer.endings(pet,timer, 'disappear_s') if stupid? && rand(1..10) == 4
   end
 
   def put_to_sleep(pet, timer)
     timer.day_time = 1
     puts "You put sleep #{pet.name} and now #{timer.day_period}"
     timer.status_counting(pet, timer, 'sleeping')
+    danger_notification(pet)
     status(pet, timer)
   end
 
@@ -87,15 +120,17 @@ class Pet
     timer.day_time += 1
     puts "You train #{pet.name} and now #{timer.day_period}"
     timer.status_counting(pet, timer, 'power_up')
+    danger_notification(pet)
     status(pet, timer)
   end
-  def geme_over
-
+  def geme_over(pet,timer)
+    timer.endings(pet, timer, 'die') if over?
   end
+
 
   private
   def over?
-    @hp <=0
+    @hp <= 0
   end
 
   def hungry?
@@ -118,31 +153,30 @@ class Pet
   end
 
   def danger_notification(pet)
-    puts "#{pet.name} hp is #{pet.hp} and it's to low. Feed #{pet.name}" if ill?
-    puts "#{pet.name} stomach is #{pet.stomach} and it's to low. Feed #{pet.name}" if hungry?
-    puts "#{pet.name} purity is #{pet.purity} and it's to low. Wash #{pet.name}" if dirty?
-    puts "#{pet.name} interest is #{pet.interest} and it's to low. Play or walk with #{pet.name} " if interest?
-    puts "#{pet.name} sleepiness is #{pet.sleepiness} and it's to low. #{pet.name} need a rest " if sleepy?
-    puts "#{pet.name} intelect is #{pet.intelect} and it's to low. Train #{pet.name}" if stupid?
+    puts "#{pet.name} hp is #{pet.hp} and it's to low. Feed #{pet.name}".red if ill?
+    puts "#{pet.name} stomach is #{pet.stomach} and it's to low. Feed #{pet.name}".red if hungry?
+    puts "#{pet.name} purity is #{pet.purity} and it's to low. Wash #{pet.name}".red if dirty?
+    puts "#{pet.name} interest is #{pet.interest} and it's to low. Play or walk with #{pet.name} ".red if interest?
+    puts "#{pet.name} sleepiness is #{pet.sleepiness} and it's to low. #{pet.name} need a rest ".red if sleepy?
+    puts "#{pet.name} intelect is #{pet.intelect} and it's to low. Train #{pet.name}".red if stupid?
   end
 
   def status(pet, timer)
+
     puts "
-
+         #{pet.render}
 #{pet.name}  status
-
 
 Time = #{timer.day_period}
 ---------------------------------------------
 |hp| = #{pet.hp}     |stomach| = #{pet.stomach}    |purity| = #{pet.purity}
 |interes| = #{pet.interest} |sleepiness| = #{pet.sleepiness} |intelect| = #{pet.intelect}
-#{danger_notification(pet)}
---------------------------------------------- "
+--------------------------------------------- ".cyan
   end
 end
 
 class Timer
-  attr_accessor :day_time
+  attr_accessor :day_time, :game_over
   attr_reader :max_hp, :max_interest, :max_stomach, :max_sleepiness, :max_intelect, :max_purity, :finish_game
 
   def initialize(day_time)
@@ -154,12 +188,24 @@ class Timer
     @max_intelect = 6
     @max_purity = 4
     @day_count = 0
-    @finish_game = false
+
   end
 
 
-  def endings(pet)
-    ending_title(pet, 'die') if pet.hp == 0
+  def endings(pet, timer, reason)
+    $game_over = true
+    case reason
+    when 'die'
+
+      timer.ending_title(pet, timer, 'die')
+    when 'disappear_b'
+
+      timer.ending_title(pet, timer, 'disappear_b')
+    when 'disappear_s'
+
+      timer.ending_title(pet, timer, 'disappear_s')
+    end
+
   end
 
   def random_actions(pet, actions, timer)
@@ -185,63 +231,102 @@ class Timer
         pet.stomach -= 3 if pet.stomach > 3
         pet.stomach = 0 if pet.stomach < 3
         pet.hp -= 1
-        puts "When you walking your pet found trash and eat it. It's hurts...."
+        puts "When you walking your pet found trash and eat it. It's hurts....".bg_brown.black.italic
       when 3
         pet.stomach += 3
         pet.stomach = timer.max_stomach if pet.stomach < timer.max_stomach - 3
         pet.hp += 1 if pet.hp != timer.max_hp
-        puts "When you walking your pet normal food. Next time be careful you may catch something dangerous"
+        puts "When you walking your pet normal food. Next time be careful you may catch something dangerous".bg_brown.black.italic
       when 5
         pet.stomach = timer.max_stomach
         pet.hp = timer.max_hp
-        puts "During your walkin #{pet.name} breathed freash air and it feel nice"
+        puts "During your walkin #{pet.name} breathed freash air and it feel nice".bg_brown.black.italic
       end
     when 'power_up'
       puts situation
       case situation
       when 1
         pet.hp -= 2
-        puts "When you trained your pet, you were inattentive. It hurts .... "
+        puts "When you trained your pet, you were inattentive. It hurts .... ".bg_brown.black.italic
       when 3
         pet.intelect += 1 if pet.intelect <= timer.max_intelect - 1
         pet.hp += 1 if pet.hp != timer.max_hp - 1
-        puts "Your training is successful. Get extra bonus"
+        puts "Your training is successful. Get extra bonus".bg_brown.black.italic
       when 5
         pet.intelect += 2 if pet.intelect <= timer.max_intelect  - 3
         pet.hp += 2 if pet.hp != timer.max_hp - 2
-        puts "Your training is very successful. Get mega bonus"
+        puts "Your training is very successful. Get mega bonus".bg_brown.black.italic
       end
     when 'playing'
       case
     when 1
       pet.hp -= 2
-      puts "When you play with your pet, you were inattentive. It hurts .... "
+      puts "When you play with your pet, you were inattentive. It hurts .... ".bg_brown.black.italic
     when 3
       pet.intelect += 1 if pet.intelect <= timer.max_intelect - 1
       pet.hp += 1 if pet.hp != timer.max_hp - 1
-      puts "Your playing is successful. Get extra bonus"
+      puts "Your playing is successful. Get extra bonus".bg_brown.black.italic
       when 5
       pet.interest +=1 if pet.interest <= timer.max_interest - 2
       pet.intelect += 1 if pet.intelect <= timer.max_intelect  - 2
       pet.hp += 2 if pet.hp != timer.max_hp - 2
-      puts "Your playing is very successful. Get mega bonus"
+      puts "Your playing is very successful. Get mega bonus".bg_brown.black.italic
     end
     end
   end
 
-  def ending_title(pet, reson)
-    case reson
+  def ending_title(pet, timer, reason)
+    case reason
     when 'die'
+
       puts "#{pet.name} die
       ,-=-.
      /  +  l
      | ~~~ |
      |R.I.P|
      |_____|
-"
-      finish_game = true
-    when 'disapere'
+".magenta if $game_over
+
+    when 'disappear_b'
+
+      puts " On walkink #{pet.name} get away because it bored
+ _____   ___  ___  ___ _____
+|  __ l / _ l |  l/  ||  ___|
+| |  l// /_l l| .  . || |__
+| | __ |  _  || |l/| ||  __|
+| |_l l| | | || |  | || |___
+ l____/l_| |_/l_|  |_/l____/
+
+
+ _____  _   _ ___________
+|  _  || | | |  ___| ___ l
+| | | || | | | |__ | |_/ /
+| | | || | | |  __||    /
+l l_/ /l l_/ / |___| |l l
+ l___/  l___/l____/l_| l_|
+".magenta if $game_over
+
+    when 'disappear_s'
+
+      puts " On walkink #{pet.name} lost because it stupid
+ _____   ___  ___  ___ _____
+|  __ l / _ l |  l/  ||  ___|
+| |  l// /_l l| .  . || |__
+| | __ |  _  || |l/| ||  __|
+| |_l l| | | || |  | || |___
+ l____/l_| |_/l_|  |_/l____/
+
+
+ _____  _   _ ___________
+|  _  || | | |  ___| ___ l
+| | | || | | | |__ | |_/ /
+| | | || | | |  __||    /
+l l_/ /l l_/ / |___| |l l
+ l___/  l___/l____/l_| l_|
+".magenta if $game_over
+
     end
+
   end
 
 
@@ -308,8 +393,8 @@ end
 # start game--------------------------------------
 
 def dayli(pet, timer)
-  if timer.day_time != 3
-  puts "---------------------------
+  if timer.day_time != 3 && $game_over != true
+  puts "
 
  Chose what you wanna do
 1.feed             5.walk
@@ -336,11 +421,11 @@ Comand: "
   else
     puts "    !!!!!!!!!!!!!!!!!!!!!!!!
     !! Type corect number !!
-    !!!!!!!!!!!!!!!!!!!!!!!!"
+    !!!!!!!!!!!!!!!!!!!!!!!!".bg_red.black
     dayli(pet, timer)
   end
-  else
-    puts "---------------------------
+  elsif  $game_over != true
+    puts "
 Its to late you can only sleep
         1.put_to_sleep
 
@@ -351,13 +436,15 @@ Comand: "
     else
        puts "    !!!!!!!!!!!!!!!!!!!!!!!!
     !! Type corect number !!
-    !!!!!!!!!!!!!!!!!!!!!!!!"
+    !!!!!!!!!!!!!!!!!!!!!!!!".bg_red.black
     end
+    else
   end
 end
 
 def start_game
-  print ' chose you animal name:  '
+  $game_over = false
+  print ' chose you animal name:  '.green.underline
   input_pet_name = gets.chomp
   pet = Pet.new('dog', input_pet_name.capitalize, 5, 6, 4, 4, 2, 6)
   timer = Timer.new(1)
@@ -368,18 +455,26 @@ Time = #{timer.day_period}
 ---------------------------------------------
 |hp| = #{pet.hp}     |stomach| = #{pet.stomach}    |purity| = #{pet.purity}
 |interes| = #{pet.interest} |sleepiness| = #{pet.sleepiness} |intelect| = #{pet.intelect}
---------------------------------------------- "
+--------------------------------------------- ".cyan
+
   game(pet, timer)
 end
 
 
 
 def game(pet, timer)
+  pet.geme_over(pet, timer)
+  unless $game_over
+    timer.danger_moments(pet)
+    dayli(pet, timer)
+    game(pet, timer)
+    end
 
-  timer.endings(pet)
-  timer.danger_moments (pet)
-  dayli(pet, timer)
-  game(pet, timer)
 end
 
 start_game
+puts "pres ENTER to start new game"
+enter_button = gets.chomp
+if enter_button == enter_button
+  start_game
+end
